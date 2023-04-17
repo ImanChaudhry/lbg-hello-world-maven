f8551cdf5bf67859cab35be1d815d21817313d1c
iman chaudhry <i-man-735@hotmail.com>
2023-04-17 19:34:43 +0100

    added Iman and Weronika names

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,8 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Iman"));
+        System.out.println(sayHelloToSomeone("Weronika"));
         System.out.println(sayGoodbye());
     }
 
